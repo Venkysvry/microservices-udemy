ea0df3f3c5a4476056b3607b19a5f918201e76de
Venkysvry <venkateshkurva46@gmail.com>
2023-12-01 09:06:04 +0530

    1st changes2

---
+++ trainer-service/src/main/java/com/trainerservice/ApiClient/StudentClient.java
@@ -0,0 +1,16 @@
+package com.trainerservice.ApiClient;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.trainerservice.entity.StudentEntity;
+
+
+@FeignClient(url = "http://localhost:8535",name ="STUDENT-SERVICE" )
+public interface StudentClient {
+	@GetMapping("/studentcontroller/findBytech/{id}")
+	 List<StudentEntity> getByTech(@PathVariable String id);
+}
